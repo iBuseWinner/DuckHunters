35c481c5bdffe6bb947d1c7d50ea8842034d27c2
Sergey <busewin@vk.com>
2024-07-07 22:24:20 +0300

    Ещё одно добавление полей игроку

--- src/main/java/ru/fennec/free/duckhunters/common/interfaces/IGamePlayer.java
+++ src/main/java/ru/fennec/free/duckhunters/common/interfaces/IGamePlayer.java
@@ -1,6 +1,7 @@
 package ru.fennec.free.duckhunters.common.interfaces;
 
 import org.bukkit.entity.Player;
+import ru.fennec.free.duckhunters.handlers.enums.PlayerRole;
 
 import java.util.Map;
 import java.util.UUID;
@@ -11,13 +12,25 @@ public interface IGamePlayer {
 
     Player getBukkitPlayer();
 
-    UUID getGamePlayerUUID();
+    UUID getPlayerUUID();
 
     //Победы, поражения, убийства, смерти
     Map<String, Long> getStatistics();
 
+    PlayerRole getPlayerRole();
+
+    boolean isSpectator();
+
+    IGame getGame();
+
     void setId(long id);
 
     void setStatistic(String name, long value);
+
+    void setPlayerRole(PlayerRole playerRole);
+
+    void setSpectator(boolean spectator);
+
+    void setGame(IGame game);
 
 }
