6631e5d99dcbf6c9293f3ac269d963a7fd14cfac
Sergey <busewin@vk.com>
2024-07-28 17:49:22 +0300

    Добавлены слушатели, они инициализируются, но не все регистрируются

--- src/main/java/ru/fennec/free/duckhunters/DuckHuntersPlugin.java
+++ src/main/java/ru/fennec/free/duckhunters/DuckHuntersPlugin.java
@@ -12,6 +12,11 @@ import ru.fennec.free.duckhunters.handlers.database.data.MySQLDatabase;
 import ru.fennec.free.duckhunters.handlers.database.data.SQLDatabase;
 import ru.fennec.free.duckhunters.handlers.DuckHuntersCommand;
 import ru.fennec.free.duckhunters.handlers.game.GameManager;
+import ru.fennec.free.duckhunters.handlers.game.GameStateListener;
+import ru.fennec.free.duckhunters.handlers.game.conventer.PlayerEventsConventer;
+import ru.fennec.free.duckhunters.handlers.game.loading.PlayerLoadingListener;
+import ru.fennec.free.duckhunters.handlers.game.playing.PlayerPlayingListener;
+import ru.fennec.free.duckhunters.handlers.game.waiting.PlayerWaitingListener;
 import ru.fennec.free.duckhunters.handlers.messages.MessageManager;
 import ru.fennec.free.duckhunters.handlers.messages.PlaceholderHook;
 import ru.fennec.free.duckhunters.handlers.players.PlayersContainer;
@@ -28,6 +33,11 @@ public final class DuckHuntersPlugin extends JavaPlugin {
     private PlaceholderHook placeholderHook;
     private GameManager gameManager;
     private WorldLoader worldLoader;
+    private GameStateListener gameStateListener;
+
+    private PlayerWaitingListener playerWaitingListener;
+    private PlayerPlayingListener playerPlayingListener;
+    private PlayerLoadingListener playerLoadingListener;
 
     @Override
     public void onEnable() {
@@ -37,6 +47,8 @@ public final class DuckHuntersPlugin extends JavaPlugin {
         //ToDo слушатели...
         registerListeners();
         registerCommand();
+
+        gameStateListener.runTaskTimer(this, 20*5, 20);
     }
 
     private void loadConfigs() {
@@ -60,9 +72,11 @@ public final class DuckHuntersPlugin extends JavaPlugin {
         this.playersContainer = new PlayersContainer(database);
         this.messageManager = new MessageManager(messagesConfigManager);
         this.worldLoader = new WorldLoader(this, this.mainConfigManager);
-        this.gameManager = new GameManager(this, getDataFolder(), worldLoader); //ToDo data folder & world loader
+        this.gameManager = new GameManager(this, getDataFolder(), worldLoader, mainConfigManager, messagesConfigManager, this.messageManager);
         this.gameManager.loadGameSettings();
         this.gameManager.prepareLimboWorld();
+        this.gameStateListener = new GameStateListener(this, this.gameManager, this.messageManager,
+                this.mainConfigManager, this.messagesConfigManager);
         if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
             this.placeholderHook = new PlaceholderHook(getPluginMeta().getVersion(), playersContainer, database, this.mainConfigManager);
             this.placeholderHook.register();
@@ -79,6 +93,13 @@ public final class DuckHuntersPlugin extends JavaPlugin {
     private void registerListeners() {
         //ToDo: register PlayerEventsConventer, GlobalPlayerListener
         // Also initialize ended, loading, playing, starting, waiting listeners but not enable them
+        getServer().getPluginManager().registerEvents(
+                new PlayerEventsConventer(this, messageManager, mainConfigManager, messagesConfigManager, playersContainer), this);
+        getServer().getPluginManager().registerEvents(gameStateListener, this);
+
+        playerWaitingListener = new PlayerWaitingListener(this, mainConfigManager, messagesConfigManager, messageManager);
+        playerPlayingListener = new PlayerPlayingListener();
+        playerLoadingListener = new PlayerLoadingListener(this, mainConfigManager, messagesConfigManager, messageManager);
     }
 
     @Override
@@ -89,10 +110,10 @@ public final class DuckHuntersPlugin extends JavaPlugin {
     }
 
     public void updateConfigData() {
-        //ToDo: update config in all files like there
-        // https://github.com/iBuseWinner/Reputation/blob/master/src/main/java/ru/fennec/free/reputation/ReputationPlugin.java#L96
         this.messageManager.updateConfigData(this.messagesConfigManager);
         this.worldLoader.updateConfigData(this.mainConfigManager);
+        this.gameManager.updateConfigData(this.mainConfigManager, this.messagesConfigManager);
+        this.gameStateListener.updateConfigData(this.mainConfigManager, this.messagesConfigManager);
     }
 
     public GameManager getGameManager() {
@@ -101,5 +122,25 @@ public final class DuckHuntersPlugin extends JavaPlugin {
 
     public WorldLoader getWorldLoader() {
         return worldLoader;
+    }
+
+    public PlayerWaitingListener getPlayerWaitingListener() {
+        return playerWaitingListener;
+    }
+
+    public PlayerPlayingListener getPlayerPlayingListener() {
+        return playerPlayingListener;
+    }
+
+    public PlayerLoadingListener getPlayerLoadingListener() {
+        return playerLoadingListener;
+    }
+
+    public IDatabase getDatabase() {
+        return database;
+    }
+
+    public PlayersContainer getPlayersContainer() {
+        return playersContainer;
     }
 }
