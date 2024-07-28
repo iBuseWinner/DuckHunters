9eddf0ec808ceb9146ad7e16f3f4d60b9ae615a1
Sergey <busewin@vk.com>
2024-07-28 17:50:51 +0300

    Добавлен Action в интеракт

--- src/main/java/ru/fennec/free/duckhunters/common/events/GamePlayerInteractEvent.java
+++ src/main/java/ru/fennec/free/duckhunters/common/events/GamePlayerInteractEvent.java
@@ -1,24 +1,30 @@
 package ru.fennec.free.duckhunters.common.events;
 
-import org.bukkit.Location;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.block.Action;
 import org.jetbrains.annotations.NotNull;
 import ru.fennec.free.duckhunters.common.interfaces.IGamePlayer;
 
 public class GamePlayerInteractEvent extends Event implements Cancellable {
     private IGamePlayer gamePlayer;
+    private Action action;
     private boolean cancelled;
 
     private static final HandlerList handlerList = new HandlerList();
 
-    public GamePlayerInteractEvent(IGamePlayer gamePlayer) {
+    public GamePlayerInteractEvent(IGamePlayer gamePlayer, Action action) {
         this.gamePlayer = gamePlayer;
+        this.action = action;
     }
 
     public IGamePlayer getGamePlayer() {
         return gamePlayer;
+    }
+
+    public Action getAction() {
+        return action;
     }
 
     @Override
